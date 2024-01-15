84145b1848fee58100d002aa74ecd567890313f5
dorydev <dory.54210@gmail.com>
2024-01-15 13:37:15 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+sudo apt-get install xorriso
+sudo apt-get install grub
+sudo apt-get install gcc
+sudo apt-get install nasm 
--- kernel.c
+++ kernel.c
@@ -1,7 +1,9 @@
 
-void kernel_main()
+int kernel_main()
 {
-    char* vMem = (char*)0xb8000;
-    vMem[0] = 'A';
-    vMem[1] = 0x07;
+    char* vMem=(char*)0xb8000;
+    vMem[0] = 'H';
+    vMem[1] = 0x04;
+    vMem[2] = 'E';
+    vMem[3] = 0x04;
 }
