1c7199cd07e9728ea6b05ab3b2627d5efb83ae69
dorydev <dory.54210@gmail.com>
2024-01-15 15:17:27 +0100

    some adds

--- kernel.c
+++ kernel.c
@@ -2,7 +2,14 @@ void kernel_main(void)
 {
     char* vMem=(char*)0xb8000;
     vMem[0] = 'H';
-    vMem[1] = 0x04;
-    vMem[2] = 'E';
-    vMem[3] = 0x04;
+    vMem[1] = 'E';
+    vMem[2] = 'L';
+    vMem[3] = 'L';
+    vMem[4] = 'O';
+    vMem[5] = ' ';
+    vMem[6] = 'W';
+    vMem[7] = 'O';
+    vMem[8] = 'R';
+    vMem[9] = 'L';
+    vMem[10] = 'D';
 }
