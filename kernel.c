a210fa40b99cd19d6a0c28adf172e4df96353abd
dorydev <dory.54210@gmail.com>
2024-01-15 15:42:22 +0100

    print "hello world" on screen

--- kernel.c
+++ kernel.c
@@ -2,14 +2,23 @@ void kernel_main(void)
 {
     char* vMem=(char*)0xb8000;
     vMem[0] = 'H';
-    vMem[1] = 'E';
-    vMem[2] = 'L';
-    vMem[3] = 'L';
-    vMem[4] = 'O';
-    vMem[5] = ' ';
-    vMem[6] = 'W';
-    vMem[7] = 'O';
-    vMem[8] = 'R';
-    vMem[9] = 'L';
-    vMem[10] = 'D';
+    vMem[1] = 0x04;
+    vMem[2] = 'E';
+    vMem[3] = 0x04;
+    vMem[4] = 'L';
+    vMem[5] = 0x04;
+    vMem[6] = 'L';
+    vMem[7] = 0x04;
+    vMem[8] = 'O';
+    vMem[9] = 0x04;
+    vMem[10] = 'W';
+    vMem[11] = 0x04;
+    vMem[12] = 'O';
+    vMem[13] = 0x04;
+    vMem[14] = 'R';
+    vMem[15] = 0x04;
+    vMem[16] = 'L';
+    vMem[17] = 0x04;
+    vMem[18] = 'D';
+    vMem[19] = 0x04;
 }
