d54fedb760c4b14654ab6d1ced931b92f2861563
dorydev <95535428+dorydev@users.noreply.github.com>
2024-02-01 10:52:43 +0100

    some changes

---
+++ kernel.c
@@ -0,0 +1,29 @@
+
+//main function
+void kernel_main(void)
+{
+
+    /*char* vMem=(char*)0xb8000;
+    vMem[0] = 'H';
+    vMem[1] = 0x04;
+    vMem[2] = 'E';
+    vMem[3] = 0x04;
+    vMem[4] = 'L';
+    vMem[5] = 0x04;
+    vMem[6] = 'L';
+    vMem[7] = 0x04;
+    vMem[8] = 'O';
+    vMem[9] = 0x04;
+    vMem[10] = 'W';
+    vMem[11] = 0x04;
+    vMem[12] = 'O';
+    vMem[13] = 0x04;
+    vMem[14] = 'R';
+    vMem[15] = 0x04;
+    vMem[16] = 'L';
+    vMem[17] = 0x04;
+    vMem[18] = 'D';
+    vMem[19] = 0x04;*/
+
+    //while(1);
+}
