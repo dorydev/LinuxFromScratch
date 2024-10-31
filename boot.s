7b52e7404ee690a71c0510015fa978f3026972ca
dorydev <95535428+dorydev@users.noreply.github.com>
2024-10-31 10:50:08 +0100

    update boot.s

--- boot.s
+++ boot.s
@@ -1,8 +1,8 @@
 section .multiboot
     align 4
-    dd 0x1BADB002            ; Magic number pour Multiboot
-    dd 0x0                   ; Flags
-    dd -(0x1BADB002 + 0x0)   ; Checksum
+    dd 0x1BADB002            
+    dd 0x0                  
+    dd -(0x1BADB002 + 0x0)   
 
 section .text
 bits 32
@@ -10,6 +10,6 @@ global _start
 extern kernel_main
 
 _start:
-    cli                   ; Désactive les interruptions
-    call kernel_main      ; Appelle la fonction principale du noyau
-    hlt                   ; Arrête le processeur
+    cli                   
+    call kernel_main      
+    hlt                   
